fd1166da2b94bd1376c2cea421efb3ead5eb8d02
VEDANTI-UTTARWAR <76436260+VEDANTI-UTTARWAR@users.noreply.github.com>
2021-06-25 13:07:40 +0530

    Add files via upload

---
+++ start.js
@@ -0,0 +1,8 @@
+var box = document.getElementById('box');
+
+//function myFunction() {
+//document.getElementById("box").style.width = "300px";
+ //  document.getElementById("box").style.height = "200px";
+   //document.getElementById("box").style.background: "#000";
+    
+//}
