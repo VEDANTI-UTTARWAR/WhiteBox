5de1191b915bc9f592b1d23ffd9af493be6d9f2d
VEDANTI-UTTARWAR <76436260+VEDANTI-UTTARWAR@users.noreply.github.com>
2021-06-28 14:21:43 +0530

    Add files via upload

---
+++ content.js
@@ -0,0 +1,5 @@
+   var tag = document.createElement("p");
+   var text = document.createTextNode(".");
+   tag.appendChild(text);
+   var element = document.getElementById("new");
+   element.appendChild(tag);
