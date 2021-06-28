abf1c4957134c53d9d70f84fa3930844a093c022
VEDANTI-UTTARWAR <76436260+VEDANTI-UTTARWAR@users.noreply.github.com>
2021-06-28 14:20:37 +0530

    Delete start.js

--- start.js
+++
@@ -1,8 +0,0 @@
-var box = document.getElementById('box');
-
-//function myFunction() {
-//document.getElementById("box").style.width = "300px";
- //  document.getElementById("box").style.height = "200px";
-   //document.getElementById("box").style.background: "#000";
-    
-//}
