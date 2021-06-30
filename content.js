2533a252e7d8b3c52256ddb31e2b22b7c440434f
VEDANTI-UTTARWAR <76436260+VEDANTI-UTTARWAR@users.noreply.github.com>
2021-06-30 12:07:16 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,17 @@
-var div = document.createElement("div");
-div.style.width = "400px";
-div.style.height = "200px";
-div.style.background = "lightblue";
-document.body.appendChild(div);
+if( document.readyState !== 'loading' ) {
+    console.log( 'document is already ready, just execute code here' );
+    myInitCode();
+} else {
+    document.addEventListener('DOMContentLoaded', function () {
+        console.log( 'document was not ready, place code here' );
+        myInitCode();
+    });
+}
+
+function myInitCode() {
+var myElement = document.createElement("div");
+myElement.style.width = "400px";
+myElement.style.height = "200px";
+myElement.style.background = "lightblue";
+document.body.appendChild(myElement);
+}
